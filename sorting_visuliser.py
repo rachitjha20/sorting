997e946522014e633aaa60993f4387fd646b0cc4
rachitjha20 <rachitjha20@gmail.com>
2024-08-28 23:41:17 +0530

    added selection sort

--- sorting_visuliser.py
+++ sorting_visuliser.py
@@ -13,12 +13,44 @@ def bubble_sort():
         for j in range(0, n-i-1):
             if lst[j] > lst[j+1]:
                 lst[j], lst[j+1] = lst[j+1], lst[j]
+                plt.xlabel(f"{lst}")
+            plt.title("Bubble Sort")
             plt.bar(x, lst, color='blue')
             plt.pause(0.05)
             plt.clf()
-
+    plt.title("Bubble Sort")
+    plt.xlabel(f"{lst}")
     plt.bar(x, lst, color='green')  # Final sorted list in green
     plt.show()
 
 
-bubble_sort()
+
+
+def selection_sort():
+    
+    for i in range(n):
+        min_value = lst[i]
+        min_index = i
+        for j in range(i+1, n):
+            if lst[j] < min_value:
+                min_value = lst[j]
+                min_index = j
+                plt.xlabel(f"{lst}")
+        lst[i], lst[min_index] = lst[min_index], lst[i]
+        plt.title("Selection Sort")
+        plt.bar(x, lst, color='blue')
+        plt.pause(0.05)
+        plt.clf()
+    plt.title("Selection Sort")
+    plt.xlabel(f"{lst}")    
+    plt.bar(x, lst, color='green')  # Final sorted list in green
+    plt.show()
+    
+a = int(input("What sorting would you opt for \n 1. Bubble Sort \n 2. Selection Sort : \n"))
+
+if a == 1:
+    bubble_sort()
+elif a == 2:
+    selection_sort()
+else:
+    print('Enter correct option in numeric form.')
